51d2f5a66bcef892fbf14affc57b6c9e9072571c
BrutchsamaJeanLouis <73585384+BrutchsamaJeanLouis@users.noreply.github.com>
2024-02-14 23:33:07 +0000

    wip

--- src/client/pages/AboutLandlord.tsx
+++ src/client/pages/AboutLandlord.tsx
@@ -9,6 +9,7 @@ const AboutLandlord = props => {
       We find you reliable tenants and ensure your proprietary stays in good condition. At TCI Homebase, we offer more than simply listing your properties.
       Browse our list of services available here. (Make “here” a Hyperlink that takes them to the Products and Services Page) Register Now (Make this a
       clickable button that take them to the sign up form)
+      <div className="text-danger mt-5">This Page is work in progress</div>
     </div>
   );
 };
