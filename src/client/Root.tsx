35c2cc1b5eb023c4deb5a3d594f572ceb3fc049d
BrutchsamaJeanLouis <73585384+BrutchsamaJeanLouis@users.noreply.github.com>
2024-04-27 14:41:49 +0100

    ios android service worker update listener

--- src/client/Root.tsx
+++ src/client/Root.tsx
@@ -2,6 +2,14 @@ import React from "react";
 import Main from "./Main";
 import { ContextWrapper } from "./Context";
 
+window.addEventListener("activate", function (event: any) {
+  event?.waitUntil(
+    caches.keys().then(function (names) {
+      for (let name of names) caches.delete(name);
+    })
+  );
+});
+
 export const Root = () => {
   return (
     <ContextWrapper>
