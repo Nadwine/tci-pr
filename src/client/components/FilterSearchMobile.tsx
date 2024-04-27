24d04ed77660a13bcc977876428a0bf84fc47bed
BrutchsamaJeanLouis <73585384+BrutchsamaJeanLouis@users.noreply.github.com>
2024-04-27 14:04:50 +0100

    better auto complete fix

--- src/client/components/FilterSearchMobile.tsx
+++ src/client/components/FilterSearchMobile.tsx
@@ -27,7 +27,9 @@ const FilterSearchMobile = (props: MobileFilterSearchProps) => {
   const [showAutoComplete, setShowAutoComplete] = useState(false);
 
   useEffect(() => {
-    const shouldDisplay = Boolean(searchText && autoCompleteFilterWithSearch.length > 1);
+    const shouldDisplay = Boolean(
+      searchText && autoCompleteFilterWithSearch.length !== 0 && autoCompleteFilterWithSearch[0].toLocaleLowerCase() !== searchText.toLocaleLowerCase()
+    );
     setShowAutoComplete(shouldDisplay);
   }, [searchText]);
 
